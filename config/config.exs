70a7a071830db40b208e80365368f83cf413656a
Adrian Irving-Beer <wisq@wisq.net>
2019-03-01 04:09:00 -0500

    Use local tmpdir.

--- config/config.exs
+++ config/config.exs
@@ -5,3 +5,8 @@ config :hound, driver: "chrome_driver"
 config :logger, :console,
   # level: :info,
   format: "[$level] $message\n"
+
+config :briefly,
+  directory: [{:system, "TMPDIR"}, "tmp", "/tmp"],
+  default_prefix: "briefly",
+  default_extname: ""
